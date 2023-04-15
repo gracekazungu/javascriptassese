07fe5ac91d291341af09f54e06d13a3b0b6d2fff
gracekazungu <kazungungrace@gmail.com>
2023-04-15 20:49:37 +0300

    commit

--- redo.js
+++ redo.js
@@ -65,16 +65,23 @@ console.log(divideArr(num))
 
 // Given years between 2000 and 2023, console all the leap years in the following
 // sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
-let years=[2000,2001,2002,2003];
-for(let year in years)
-if(year%2==0){
-    console.log("is a leap year")
+// let years=[2000,2001,2002,2003];
+// for(let year in years)
+// if(year%2==0){
+//     console.log("is a leap year")
 
+// }
+// else{
+//     console.log("is not a leap year")
+// }
+for(let year=2000;year<=2023;year++){
+if(year%4==0){
+    console.log(year+ "is a leap year")
 }
 else{
-    console.log("is not a leap year")
+    console.log(year+ "is not a leap year")
 }
-
+}
 // Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
 // “Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
 
